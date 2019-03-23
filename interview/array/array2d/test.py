062175cd2cc56c953ccd272dfa4867cbfe888ba5
davisr137 <davisr137@gmail.com>
2019-03-22 18:55:43 -0700

    clean up unit testing code

--- interview/array/array2d/test.py
+++ interview/array/array2d/test.py
@@ -1,20 +1,22 @@
 #!/bin/python3
 
+import unittest2 as unittest
+
 import hackerrank.utils.unit_test as hut
 from submit import hourglassSum
 
-class TestHourglassSum(hut.TestCase):
+class TestCase(unittest.TestCase):
     """
     Test hourglass sum solution.
     """
     def test_hourglass_sum(self):
-        input_fns = TestHourglassSum.get_input_filenames()
-        output_fns = TestHourglassSum.get_output_filenames()
-        test_names = TestHourglassSum.get_test_names(input_fns, output_fns)
+        input_fns = hut.get_input_filenames()
+        output_fns = hut.get_output_filenames()
+        test_names = hut.get_test_names(input_fns, output_fns)
         for test_name in test_names:
-            input_fn = TestHourglassSum.build_input_filename(test_name)
-            output_fn = TestHourglassSum.build_output_filename(test_name)
-            arr = TestHourglassSum.read_input(input_fn)
+            input_fn = hut.build_input_filename(test_name)
+            output_fn = hut.build_output_filename(test_name)
+            arr = hut.read_input(input_fn)
             res = hourglassSum(arr)
-            expected = TestHourglassSum.read_output(output_fn)
+            expected = hut.read_output(output_fn)
             self.assertEqual(res, expected)
--- utils/unit_test.py
+++ utils/unit_test.py
@@ -3,99 +3,87 @@
 import unittest2 as unittest
 import os
 
-class TestCase(unittest.TestCase):
+def get_input_filenames():
     """
-    Shell class for test case for unit test. Use as a base class
-    for implementation of individual challenges.
+    Get all input filenames.
+
+    Returns:
+        list of str
     """
-    @staticmethod
-    def get_input_filenames():
-        """
-        Get all input filenames.
+    return os.listdir('input/')
 
-        Returns:
-            list of str
-        """
-        return os.listdir('input/')
+def get_output_filenames():
+    """
+    Get all output filenames.
 
-    @staticmethod
-    def get_output_filenames():
-        """
-        Get all output filenames.
+    Returns:
+        list of str
+    """
+    return os.listdir('output/')
 
-        Returns:
-            list of str
-        """
-        return os.listdir('output/')
+def get_test_names(input_fn, output_fn):
+    """
+    Get all names of tests. Define a 'name' as the number given
+    to the test (e.g. filenames 'input03.txt' and 'output03.txt'
+    correspond to a test name '00.txt').
 
-    @staticmethod
-    def get_test_names(input_fn, output_fn):
-        """
-        Get all names of tests. Define a 'name' as the number given
-        to the test (e.g. filenames 'input03.txt' and 'output03.txt'
-        correspond to a test name '00.txt').
+    Args:
+        input_fn (list of str): Input filenames (e.g. ['input00.txt'])
+        output_fn (list of str): Output filenames (e.g. ['output00.txt'])
 
-        Args:
-            input_fn (list of str): Input filenames (e.g. ['input00.txt'])
-            output_fn (list of str): Output filenames (e.g. ['output00.txt'])
+    Returns:
+        list of str: Names of common tests.
+    """
+    input_names = [fn.replace('.txt', '').replace('input','') for fn in input_fn]
+    output_names = [fn.replace('.txt', '').replace('output','') for fn in output_fn]
+    test_names = list((set(input_names) & set(output_names)))
+    return test_names
 
-        Returns:
-            list of str: Names of common tests.
-        """
-        input_names = [fn.replace('.txt', '').replace('input','') for fn in input_fn]
-        output_names = [fn.replace('.txt', '').replace('output','') for fn in output_fn]
-        test_names = list((set(input_names) & set(output_names)))
-        return test_names
+def build_input_filename(test_name):
+    """
+    Build input filename. 
+    Args:
+        test_name (str): Name of test.
+    Returns:
+        str: Filename corresponding to test input.
+    """
+    return 'input/input%s.txt' % test_name
 
-    @staticmethod
-    def build_input_filename(test_name):
-        """
-        Build input filename. 
-        Args:
-            test_name (str): Name of test.
-        Returns:
-            str: Filename corresponding to test input.
-        """
-        return 'input/input%s.txt' % test_name
+def build_output_filename(test_name):
+    """
+    Build output filename.
+    Args:
+        test_name (str): Name of test.
+    Returns:
+        str: Filename corresponding to test expected output.
+    """
+    return 'output/output%s.txt' % test_name
 
-    @staticmethod
-    def build_output_filename(test_name):
-        """
-        Build output filename.
-        Args:
-            test_name (str): Name of test.
-        Returns:
-            str: Filename corresponding to test expected output.
-        """
-        return 'output/output%s.txt' % test_name
+def read_input(filename):
+    """
+    Read input data (array-like).
 
-    @staticmethod
-    def read_input(filename):
-        """
-        Read input data (array-like).
+    Args: 
+        filename (str): Path to input file.
 
-        Args: 
-            filename (str): Path to input file.
+    Returns:
+        list of list of int: Input data.
+    """
+    with open(filename, 'r') as f:
+        arr = f.readlines()
+    arr = [list(map(int, x.rstrip().split())) for x in arr]
+    return arr
 
-        Returns:
-            list of list of int: Input data.
-        """
-        with open(filename, 'r') as f:
-            arr = f.readlines()
-        arr = [list(map(int, x.rstrip().split())) for x in arr]
-        return arr
+def read_output(filename):
+    """
+    Read output data (integer).
 
-    @staticmethod
-    def read_output(filename):
-        """
-        Read output data (integer).
+    Args:
+        filename (str): Path to output file.
 
-        Args:
-            filename (str): Path to output file.
-
-        Returns:
-            int: Output data.
-        """
-        with open(filename, 'r') as f:
-            out = int(f.readlines()[0])
-        return out
+    Returns:
+        int: Output data.
+    """
+    with open(filename, 'r') as f:
+        out = int(f.readlines()[0])
+    return out
