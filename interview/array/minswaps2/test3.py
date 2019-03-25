c198f5f38b1ecfe774d53d5e83fe91d1623f7325
davisr137 <davisr137@gmail.com>
2019-03-25 14:33:25 -0700

    WIP checkpoint

--- interview/array/minswaps2/test3.py
+++ interview/array/minswaps2/test3.py
@@ -6,27 +6,53 @@ import logging
 import hackerrank.utils.unit_test as hut
 
 
+def normalize_array(array, val):
+    """
+    Normalize array after a value is removed. For each element
+    greater than 'val', subtract one from its value.
+
+    Args: 
+        array (list of int): Array post-removal of one element.
+        val (int): Value removed from array.
+
+    Returns:
+        list of int: Normalized array of consecutive (not necessarily
+            sorted) integers.
+    """
+    for i, _ in enumerate(array):
+        if array[i] > val:
+            array[i] -= 1
+    return array
+
+
 class Array(object):
     """
     Class to keep track of 'state' of task (i.e. the array).
     """
-    def __init__(self, arr, arr_ordered=None):
+    def __init__(self, arr, arr_sorted=None):
         """
-        Initialize unordered array. Sort array to get.
+        Initialize unordered array. 
 
         Args:
             arr (list of int): Our array
-            arr_ordered(list of int): Ordered version of array. If None,
+            arr_sorted(list of int): Ordered version of array. If None,
                 sort 'arr' to get ordered array.
         """
-        if arr_ordered is None:
-            arr_ordered = sorted(arr)
+        if arr_sorted is None:
+            arr_sorted = sorted(arr)
+        removed = list()
         for i, val in enumerate(arr):
-            if val == arr_ordered[i]:
+            if val == arr_sorted[i]:
+                removed.append(val)
+        if len(removed) > 0:
+            removed = sorted(removed, reverse=True)
+            for val in removed:
                 arr.remove(val)
-                arr_ordered.remove(val)
+                arr_sorted.remove(val)
+                arr = normalize_array(arr, val)
+                arr_sorted = normalize_array(arr_sorted, val)
         self.arr = arr
-        self.arr_ordered = arr_ordered
+        self.arr_sorted = arr_sorted
 
     def swap(self, i, j):
         """
@@ -35,19 +61,25 @@ class Array(object):
         Args:
             i (int): Index of first element to swap.
             j (int): Index of second element to swap.
+        
+        Returns:
+            Array: Array with elements i and j swapped.
         """
         arr_swp = self.arr.copy()
         tmp = arr_swp[j]
         arr_swp[j] = arr_swp[i]
         arr_swp[i] = tmp
-        return Array(arr_swp, arr_ordered=self.arr_ordered)
+        return Array(arr_swp, arr_sorted=self.arr_sorted)
+
 
 def increment_score(score, swp_idx):
     """
     Increment score by one. 
 
     Args:
-        score (dict)
+        score (dict): Each key (int) represents an index
+            in an array. Each value (int) repreents an increase 
+            in sorted values from swapping.
         swp_idx (tuple)
     """
     if swp_idx not in score:
@@ -66,12 +98,10 @@ def generate_possible_swaps(arr):
     l = len(arr.arr)
     score = dict()
     for i in range(0, l):
-        for j in range(i+1, l):
-            swp_idx = tuple([i, j])
-            if arr.arr[i] == arr.arr_ordered[j]:
-                score = increment_score(score, swp_idx)
-            if arr.arr[j] == arr.arr_ordered[i]:
-                score = increment_score(score, swp_idx)
+        if arr.arr[i] == i+1:
+            score[i] = 2
+        else:
+            score[i] = 1
     return score
 
 
@@ -90,8 +120,9 @@ def minimum_swaps(arr):
     swaps = 0
     while len(array.arr) > 1:
         score = generate_possible_swaps(array)
-        move = max(score, key=score.get) 
-        array = array.swap(move[0], move[1])
+        i = max(score, key=score.get) 
+        j = array.arr[i]-1
+        array = array.swap(i, j)
         swaps += 1
     return swaps
 
