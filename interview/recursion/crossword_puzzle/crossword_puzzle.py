b212fbdde34a70699dc186bbab3df35705fa9f36
davisr137 <davisr137@gmail.com>
2019-06-17 16:48:24 -0700

    backtracking solution to crossword puzzle

--- interview/recursion/crossword_puzzle/crossword_puzzle.py
+++ interview/recursion/crossword_puzzle/crossword_puzzle.py
@@ -10,7 +10,7 @@ from typing import List
 
 # https://www.hackerrank.com/challenges/crossword-puzzle
 
-# Use similar backtracking approach as N-Queens.
+## Use backtracking to solve crossword puzzle. 
 
 M = 10
 
@@ -22,13 +22,13 @@ def readPuzzleInput(fn: str) -> List:
     with open(fn, 'r') as f:
         # Read first ten lines as crossword puzzle
         for _ in range(M):
-            crossword += [f.readline().strip()]
+            crossword += [list(f.readline().strip())]
         words = f.readline().strip().split(';')
     return [crossword, words]
 
 def findHorizonalBlanks(crossword: List[List[str]]) -> List:
     """
-    Find horizontal blanks in crossword puzzle.
+    Find horizontal blanks on board.
     """
     B = []
     for i in range(M):
@@ -48,7 +48,7 @@ def findHorizonalBlanks(crossword: List[List[str]]) -> List:
 
 def findVerticalBlanks(crossword: List[List[str]]) -> List:
     """
-    Find vertical blanks in crossword puzzle.
+    Find vertical blanks on board.
     """
     B = []
     for j in range(M):
@@ -67,11 +67,148 @@ def findVerticalBlanks(crossword: List[List[str]]) -> List:
     return B
 
 def findBlanks(crossword: List[List[str]]) -> List:
+    """
+    Find horizontal and vertical contiguous blank
+    spaces on board.
+    """
     return findHorizonalBlanks(crossword) + findVerticalBlanks(crossword)
 
-# Complete the crosswordPuzzle function below.
-def crosswordPuzzle(crossword, words):
-    pass
+def writeWord(crossword: List[List[str]], word: str, blank: List) -> List[List[str]]:
+    """
+    Write word to board.
+    """
+    start = blank[2]
+    end = blank[3]
+    if blank[0] == 'H':
+        i = blank[1]
+        k = 0
+        for j in range(start, end+1):
+            crossword[i][j] = word[k]
+            k += 1
+    elif blank[0] == 'V':
+        j = blank[1]
+        k = 0
+        for i in range(start, end+1):
+            crossword[i][j] = word[k]
+            k += 1
+    return crossword
+
+def writeBlank(crossword: List[List[str]], blank: List) -> List[List[str]]:
+    """
+    Write blank spaces to board.
+    """
+    start = blank[2]
+    end = blank[3]
+    if blank[0] == 'H':
+        i = blank[1]
+        for j in range(start, end+1):
+            crossword[i][j] = '-'
+    elif blank[0] == 'V':
+        j = blank[1]
+        for i in range(start, end+1):
+            crossword[i][j] = '-'
+    return crossword
+
+def validWord(crossword: List[List[str]], word: str, blank: List) -> bool:
+    """
+    Check that word can be placed in blank in valid way.
+    """
+    Lw = len(word)
+    start = blank[2]
+    end = blank[3]
+    if Lw != end - start + 1:
+        return False
+    if blank[0] == 'H':
+        i = blank[1]
+        k = 0
+        for j in range(start, end+1):
+            if crossword[i][j] not in set([word[k], '-']):
+                return False
+            k += 1
+    elif blank[0] == 'V':
+        j = blank[1]
+        k = 0
+        for i in range(start, end+1):
+            if crossword[i][j] not in set([word[k], '-']):
+                return False
+            k += 1
+    return True
+
+def solveg(crossword, words, blanks):
+    """
+    Solve using a generator function with recursion.
+    """
+    if words:
+        # Continue filling in crossword
+        word = words[0]
+        for j in range(len(blanks)):
+            blank = blanks[j]
+            if validWord(crossword, word, blank):
+                crossword = writeWord(crossword, word, blank)
+                blanks_sm = blanks[:j] + blanks[j+1:]
+                for sol in solveg(crossword, words[1:], blanks_sm):
+                    yield sol
+                crossword = writeBlank(crossword, blank) 
+    else:
+        # No words or blanks left - done!
+        yield crossword
+
+
+def solve(crossword: List[List[str]], words: List[str], blanks: List) -> List[List[str]]:
+    """
+    Solve crossword puzzle using backtracking. 
+
+    If it is possible to solve puzzle, return solution board. Else, 
+    return False.
+    """
+    # No words or blanks left - done!
+    if not words:
+        return crossword
+
+    # Continue filling out board by inserting 
+    # first word in list.
+    word = words[0]
+
+    # Try each remaining blank space.
+    for j in range(len(blanks)):
+        blank = blanks[j]
+
+        # If word placement is valid, recursively call 
+        # to insert remaining words.
+        if validWord(crossword, word, blank):
+
+            # Write word to crossword
+            crossword = writeWord(crossword, word, blank)
+            
+            # Recurse with word and blank removed
+            blanks_sm = blanks[:j] + blanks[j+1:]
+            sol = solve(crossword, words[1:], blanks_sm)
+            if sol:    
+                return sol
+
+            # If word placement does not lead to a solution, write
+            # blank back.
+            crossword = writeBlank(crossword, blank)
+
+    return False
+
+## Utility functions to convert str <-> List
+
+def strToList(s: str) -> List[str]:
+    return [list(l) for l in s]
+
+def listToStr(L: List) -> str:
+    return ["".join(l) for l in L]
+
+def crosswordPuzzle(crossword: List[List[str]], words: List[str]) -> List[List[str]]:
+    """
+    Complete the crossword puzzle.
+    """
+    crossword = strToList(crossword)
+    blanks = findBlanks(crossword)
+    crossword = solve(crossword, words, blanks)
+    crossword = listToStr(crossword)
+    return crossword
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
@@ -79,7 +216,7 @@ if __name__ == '__main__':
     for _ in range(M):
         crossword_item = input()
         crossword.append(crossword_item)
-    words = input()
+    words = input().strip().split(';')
     result = crosswordPuzzle(crossword, words)
     fptr.write('\n'.join(result))
     fptr.write('\n')
